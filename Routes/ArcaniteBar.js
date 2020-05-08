e7df96d91973679996a6fd47c579be1a54f79352
AJ <alexanderjdiaz89@gmail.com>
2020-05-08 10:40:18 -0600

    clean up index and router files

--- Routes/ArcaniteBar.js
+++ Routes/ArcaniteBar.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
-const Nexus = require('nexushub-client');
-const nexus = new Nexus({});
+const Nexus = require('nexushub-client')
+const nexus = new Nexus({})
 
 router.get("/", async function (req, res) {
     const data = await nexus.get("/wow-classic/v1/items/kromcrush-horde/12360")
--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const express = require("express");
-const app = express();
-const cors = require('cors');
-// const Nexus = require('nexushub-client');
+const express = require("express")
+const app = express()
+const cors = require('cors')
 const blackLotus = require("./routes/BlackLotus.js")
 const mooncloth = require("./Routes/MoonCloth")
 const arcaniteBar = require("./Routes/ArcaniteBar")
 
 // CORS
 app.use(cors())
-
 app.get('/products/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
   })
-   
 app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
   })
-
-
+  
   // WoW Data
 app.use("/BlackLotus", blackLotus)
 app.use("/MoonCloth", mooncloth)
@@ -26,8 +22,6 @@ app.use("/ArcaniteBar", arcaniteBar)
 var server = app.listen(9000, function () {
     var host = server.address().address
     var port = server.address().port
-    
     console.log("Example app listening at http://%s:%s", host, port)
-
 })
 
