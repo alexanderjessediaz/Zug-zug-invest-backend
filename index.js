3f84843550ec40dd38c8c46b35d92350b43b0af6
AJ <alexanderjessediaz@gmail.com>
2020-06-13 17:21:52 -0600

    add nodemon uniform string on index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express")
 const app = express()
-const cors = require('cors')
-const blackLotus = require("./routes/BlackLotus")
+const cors = require("cors")
+const blackLotus = require("./Routes/BlackLotus")
 const mooncloth = require("./Routes/MoonCloth")
 const arcaniteBar = require("./Routes/ArcaniteBar")
 const woolcloth = require("./Routes/WoolCloth")
 
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+
+const createUser = (req, res) => {
+  response.json({
+    user: "Added user"
+  })
+}
+app.post("/users", createUser)
+
 // CORS
 app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
+app.get("/products/:id", function (req, res, next) {
+    res.json({msg: "This is CORS-enabled for all origins!"})
   })
 app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
+    console.log("CORS-enabled web server listening on port 80")
   })
 
   // WoW Data
