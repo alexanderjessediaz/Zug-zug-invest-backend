8edda3ccbb58e9e0d1d5dfcfacef5b95a145c2f6
AJ <alexanderjessediaz@gmail.com>
2021-03-03 12:41:31 -0700

    comment out port 80

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const blackLotus = require("./Routes/BlackLotus")
 const mooncloth = require("./Routes/MoonCloth")
 const arcaniteBar = require("./Routes/ArcaniteBar")
 const woolcloth = require("./Routes/WoolCloth")
+
+
 const mongoose = require('mongoose')
 const User = require('./models/user');
 const app = express()
@@ -33,21 +35,14 @@ const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 9000
 app.use(bodyParser.json())
 
-// const createUser = (req, res) => {
-//   response.json({
-//     user: "Added user"
-//   })
-// }
-// app.post("/users", createUser)
-
 // CORS
 app.use(cors())
-app.get("/products/:id", function (req, res, next) {
-  res.json({msg: "This is CORS-enabled for all origins!"})
-})
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80")
-})
+// app.get("/products/:id", function (req, res, next) {
+//   res.json({msg: "This is CORS-enabled for all origins!"})
+// })
+// app.listen(80, function () {
+//   console.log("CORS-enabled web server listening on port 80")
+// })
 
 // WoW Data
 app.use("/BlackLotus", blackLotus)
@@ -56,9 +51,9 @@ app.use("/ArcaniteBar", arcaniteBar)
 app.use("/WoolCloth", woolcloth)
 
 app.set("port", PORT)
-var server = app.listen(PORT, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-})
+// var server = app.listen(PORT, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log("Example app listening at http://%s:%s", host, port)
+// })
 
