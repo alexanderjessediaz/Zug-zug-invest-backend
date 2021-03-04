6bc0fb52faeb2c19acf0f093ae4c51c3f20a42c5
AJ <alexanderjessediaz@gmail.com>
2021-03-03 20:24:02 -0700

    edit index.js, move cors above route calls

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /index
 COPY package.json /index
 RUN npm install
 COPY . /index
+EXPOSE 5555
 CMD ["npm","start"]
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const mooncloth = require("./Routes/MoonCloth")
 const arcaniteBar = require("./Routes/ArcaniteBar")
 const woolcloth = require("./Routes/WoolCloth")
 const app = express()
+app.use(cors())
 
-
+const PORT = 5555
 app.use("/BlackLotus", blackLotus)
 app.use("/MoonCloth", mooncloth)
 app.use("/ArcaniteBar", arcaniteBar)
@@ -14,22 +15,15 @@ app.use("/WoolCloth", woolcloth)
 
 // const tracer = require('dd-trace').init()
 
-    
-        // const bodyParser = require("body-parser")
-        // const PORT = 9000
         
-        // app.use(bodyParser.json())
-        
-        
-app.use(cors())
 app.get("/products/:id", function (req, res, next) {
   res.json({msg: "This is CORS-enabled for all origins!"})
 })
-// app.listen(80, function () {
-//   console.log("CORS-enabled web server listening on port 80")
-// })
-          // WoW Data
-var server = app.listen(5555, function () {
+app.listen(80, function () {
+  console.log("CORS-enabled web server listening on port 80")
+})
+
+var server = app.listen(PORT, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
