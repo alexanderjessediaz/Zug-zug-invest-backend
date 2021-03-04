2f64c32a038cc4dfecbffd17765ac3141019e4d5
AJ <alexanderjessediaz@gmail.com>
2021-03-03 22:39:43 -0700

    add express datadog

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ const app = express()
 var StatsD = require('hot-shots')
 var dogstatsd = new StatsD()
 
+var dd_options = {
+  'response_code':true,
+  'tags': ['app:my_app']
+    }
+
+var connect_datadog = require('connect-datadog')(dd_options);
+
 dogstatsd.increment('page.views')
 app.use(cors())
 
 const PORT = 5555
+DD_TRACE_AGENT_PORT=PORT
 app.use("/BlackLotus", blackLotus)
 app.use("/MoonCloth", mooncloth)
 app.use("/ArcaniteBar", arcaniteBar)
 app.use("/WoolCloth", woolcloth)
-
-// const tracer = require('dd-trace').init()
+app.use(connect_datadog)
+const tracer = require('dd-trace').init()
 
         
 app.get("/products/:id", function (req, res, next) {
@@ -26,6 +34,8 @@ app.get("/products/:id", function (req, res, next) {
 app.listen(80, function () {
   console.log("CORS-enabled web server listening on port 80")
 })
+
+app.use(router)
 
 var server = app.listen(PORT, function () {
   var host = server.address().address
