63602468a3176d8c07121fc8056c110d147f69f0
AJ <alexanderjessediaz@gmail.com>
2021-03-25 10:28:24 -0600

    remove and uninstall datadog dependencies

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:14
-WORKDIR /index
-COPY package.json /index
-RUN npm install
-COPY . /index
-CMD ["node","index.js"]
--- index.js
+++ index.js
@@ -5,27 +5,14 @@ const mooncloth = require("./Routes/MoonCloth")
 const arcaniteBar = require("./Routes/ArcaniteBar")
 const woolcloth = require("./Routes/WoolCloth")
 const app = express()
-var StatsD = require('hot-shots')
-var dogstatsd = new StatsD()
 
-var dd_options = {
-  'response_code':true,
-  'tags': ['app:my_app']
-    }
-
-var connect_datadog = require('connect-datadog')(dd_options);
-
-dogstatsd.increment('page.views')
 app.use(cors())
 
 const PORT = 5555
-DD_TRACE_AGENT_PORT=PORT
 app.use("/BlackLotus", blackLotus)
 app.use("/MoonCloth", mooncloth)
 app.use("/ArcaniteBar", arcaniteBar)
 app.use("/WoolCloth", woolcloth)
-app.use(connect_datadog)
-const tracer = require('dd-trace').init()
 
         
 app.get("/products/:id", function (req, res, next) {
@@ -35,7 +22,6 @@ app.listen(80, function () {
   console.log("CORS-enabled web server listening on port 80")
 })
 
-app.use(router)
 
 var server = app.listen(PORT, function () {
   var host = server.address().address
