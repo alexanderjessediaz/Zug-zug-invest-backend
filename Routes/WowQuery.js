de3fc35eb35fa613a0fc682205bcf939235b1019
AJ <alexanderjessediaz@gmail.com>
2021-06-09 16:31:47 -0600

    clean up unnecessary folders files and comments

--- Routes/WowQuery.js
+++ Routes/WowQuery.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import Nexus from 'nexushub-client'
 const nexus = new Nexus({})
-// import { getFactionQuery, getServerQuery } from '../Controllers/QueryController.js'
+
 
 router.all("/", async (req, res) => {  
     const nexusQuery = req.body.nQuery
@@ -15,14 +15,6 @@ router.all("/", async (req, res) => {
     res.send({data})
     }
   })
-
-
-
-//   router.get("/", async function (req, res) {
-    //   const data = await nexus.get(`/wow-classic/v1/items/${selectedServer}-${selectedFaction}/13468`)
-    //   const hardCodedData = await nexus.get("/wow-classic/v1/items/kromcrush-horde/13468/prices")
-    //   res.send({ hardCodedData})
-//   })
 
  
 export default router
