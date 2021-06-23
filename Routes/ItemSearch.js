9366b5e6fcd66f0dbe286cbec04475a51c845942
AJ <alexanderjessediaz@gmail.com>
2021-06-22 20:50:13 -0600

    create ItemSearch Route to fetch API seach data

--- Routes/ItemSearch.js
+++ Routes/ItemSearch.js
@@ -4,15 +4,14 @@ import Nexus from 'nexushub-client';
 const nexus = new Nexus({});
 
 router.get('/', async function (req,res) {
-    const userServer = req.query.server;
-    const userFaction = req.query.faction;
-    const priceQuery = `/wow-classic/v1/items/${userServer}-${userFaction}/21884/prices`;
+    const userItemQuery = req.query.itemSearch
+    const searchQuery = `/wow-classic/v1/search?query=${userItemQuery}`;
+    console.log(userItemQuery)
     try {
-        res.status(200).send(await nexus.get(priceQuery));
+        res.status(200).send(await nexus.get(searchQuery));
     } catch (error) {
         console.error("Error:", error);
     }
 })
-
 
 export default router
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
 import express from 'express';
 import cors from 'cors';
-import Nexus from 'nexushub-client';
-
-const nexus = new Nexus({});
 
 import servers from './Routes/Servers.js';
-import itemPrice from './Routes/ItemPrice.js'
+import itemPrice from './Routes/ItemPrice.js';
+import itemSearch from './Routes/ItemSearch.js'
+
 
 const app = express();
 
@@ -17,41 +15,7 @@ app.use(cors());
 
 app.use("/Servers", servers);
 app.use("/ItemPrice", itemPrice)
-
-// app.post("/", async (req, res) => {  
-//   const nexusQuery = req.body.nQuery;
-//   if(!nexusQuery) {
-//       console.log("POST: no Query");
-//       res.status(400).send('bad request')
-      
-//   } else {
-//     app.set('clientQuery', nexusQuery);
-//     res.status(200).send(`${nexusQuery}: recieved and set`);
-//     console.log("Current Client Query:" , app.settings.clientQuery);
-//   }
-// });
-
-
-// app.get('/', async(req,res) => {
-  // if(app.settings.clientQuery === undefined) {
-  //   // console.log("GET: no Query", req.body);
-  //   try {
-     
-//       app.get(app.settings.clientQuery);
-//     } catch(error) {
-//       console.error(error);
-//     }
-// } else {
-//     try {
-//       app.get(app.settings.clientQuery);
-//     } 
-//     catch(error) {
-//       console.error(error);
-//     }
-//       const nData = await nexus.get(app.settings.clientQuery).catch((error) => console.error(error));
-//       res.send({nData});
-//   }
-// });
+app.use("/ItemSearch", itemSearch)
 
 
 
@@ -68,7 +32,3 @@ app.get("/products/:id", function (req, res, next) {
 app.listen(80, function () {
   console.log("CORS-enabled web server listening on port 80")
 });
-
-
-
-
