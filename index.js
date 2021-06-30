e1c57ec12fc6207d7c6b4697e715ca2088e59d82
AJ <alexanderjessediaz@gmail.com>
2021-06-29 18:01:07 -0600

    revert cors port to 8000

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ const server = app.listen(PORT, () => {
 app.get("/products/:id", function (req, res, next) {
   res.json({msg: "This is CORS-enabled for all origins!"})
 });
-app.listen(PORT, function () {
-  console.log(`CORS-enabled web server listening on port ${PORT}`)
+app.listen(8000, function () {
+  console.log(`CORS-enabled web server listening on port 8000`)
 });
